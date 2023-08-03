2b45c7bfc26b5002ce651b7f3f487bfaee02b745
Himanshu Verma <himanshuverma544@gmail.com>
2023-08-03 13:56:32 +0530

    add: created paginate component

---
+++ src/components/Paginate.js
@@ -0,0 +1,11 @@
+import { memo } from "react";
+
+import { Pagination, PaginationItem, PaginationLink } from "reactstrap";
+
+
+const Paginate = ({totalPages, fetchThePrevPage, setTheCurrentPage, fetchTheNextPage}) => {
+
+  return <></>;
+}
+
+export default Paginate;
