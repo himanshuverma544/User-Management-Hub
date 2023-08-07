ef747fbf122039c921b310cdceda058e0615c9c8
Himanshu Verma <himanshuverma544@gmail.com>
2023-08-07 14:44:53 +0530

    upd: added constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,11 @@
 const FIRST_PAGE = 1;
 
+const DEFAULT_BTN_NODE = "default-btn-node-";
+const ADMIN_BTN_NODE = "admin-btn-node-";
+
+
 export {
-  FIRST_PAGE
+  FIRST_PAGE,
+  DEFAULT_BTN_NODE,
+  ADMIN_BTN_NODE
 };
